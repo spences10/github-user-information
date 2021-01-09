e9d7741619b41d7c13eed7e0d2c2fae677f1b111
spences10 <spences10apps@gmail.com>
2021-01-09 10:20:00 +0000

    unused comment

--- api/card.ts
+++ api/card.ts
@@ -12,7 +12,6 @@ export default async function handler(
 ) {
   try {
     const { username } = parseRequests(req)
-    // const html = fs.readFileSync('pie.html')
     const data = await getGitHubData({ username })
     const { chartData } = topLanguages(data)
     const html = getHtml(chartData)
